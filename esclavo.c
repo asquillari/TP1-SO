a6408547d01dd0c1babec531c53902865b0eec1c
Gianna Nacuzzi <giannanacuzzi@MBP-de-Gianna.fibertel.com.ar>
2024-08-26 13:51:44 -0300

    agregamos funciones distintas en el esclavo

--- esclavo.c
+++ esclavo.c
@@ -7,9 +7,26 @@
 
 #define MAX_SIZE 256
 
-int main(int argc, char * argv[]){
-    char command[MAX_SIZE];
+int get_md5(char * file, char * result); //la pongo con int por si queremos mandar un error
 
+int main(int argc, char * argv[]){
+    
+    char *file = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    char md5[MAX_SIZE];
+    /*
+    // leemos el archivo de stdin
+    while ((nread = getline(&file, &len, stdin)) > 0) {
+        file[nread - 1] = '\0'; // sacamos el \n
+        get_md5(file, md5);
+    }//deberiamos hacer algo si aca hay error por getline
+    */
+    get_md5(argv[1], md5);
+    printf("%s\t%d\n", md5, getpid());
+    free(file);
+    /*
+    char command[MAX_SIZE];
     // guardo el comando de md5sum en command
     snprintf(command, MAX_SIZE, "md5sum %s", argv[1]);
 
@@ -32,6 +49,21 @@ int main(int argc, char * argv[]){
     //habría que separar el nombre del md5
     printf("MD5: %PID ESCLAVO: esclavo: %d \n", md5, getpid());
     close(3);
+    */
+    return 0;
+}
 
+int get_md5(char * file, char * result){
+    char command[MAX_SIZE] = {0};
+    snprintf(command, MAX_SIZE, "md5sum %s", file);
+    FILE * fp = popen(command, "r");
+    if(fp == NULL){
+        perror("popen");
+        return 1;
+    }
+    if(fgets(result, MAX_SIZE, fp) != NULL){
+        result[strlen(result) - 1] = '\0'; //saco el \n
+    }
+    pclose(fp);
     return 0;
 }
